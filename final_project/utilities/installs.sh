4b54ae1d868604259423a66bd59212d16095abff
Joel Stremmel <joel_stremmel@optum.com>
2019-12-02 18:06:52 -0800

    update with spark lr

--- final_project/utilities/installs.sh
+++ final_project/utilities/installs.sh
@@ -1,6 +1,9 @@
+sudo yum update -y 
+sudo install git -y
+git clone https://github.com/jstremme/DATA512-Research.git
 pip install --user jupyter
 pip install --user ipython
 pip install --user numpy
 pip install --user pandas
 pip install --user matplotlib
-pip install --user mpld3
+pip install --user scikit-learn
