ef42df49674cc7be9c73fc2656177cd15d635a0d
jstremme <jstremme@gustavus.edu>
2019-12-03 12:27:19 -0800

    remove old env files

--- final_project/utilities/launch_jupyter.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-export PYTHONPATH="/home/hadoop/.local/lib/python3.6/site-packages:$PYTHONPATH"
-export PYSPARK_DRIVER_PYTHON=/home/hadoop/.local/bin/jupyter
-export PYSPARK_DRIVER_PYTHON_OPTS='notebook --no-browser'
-export PYSPARK_PYTHON=/usr/bin/python3
-echo $PYTHONPATH
-echo $PYSPARK_DRIVER_PYTHON
-echo $PYSPARK_DRIVER_PYTHON_OPTS
-pyspark
